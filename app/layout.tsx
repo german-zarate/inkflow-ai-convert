5e910ac80518650a8342c913a9b237e3a6c3ba44
german-zarate <germanzaratevalbuena@gmail.com>
2023-09-06 15:46:52 +0600

    ✨feat(dependencies): dsfds
    ds

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import './globals.css';
 
-import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 
@@ -11,12 +10,6 @@ import { cn } from '@/lib/utils';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 // main color
-
-export const metadata: Metadata = {
-  title: 'InkFlow AI Convert',
-  description:
-    'Experience the future of handwritten content conversion with "InkFlow AI Convert". Our innovative web application, powered by cutting-edge technologies, offers a seamless solution to transform handwritten text into digital text. This pioneering solution rests upon the pillars of Next.js, Tailwind CSS, and MongoDB, forming the bedrock of innovation. Simplify your workflow, enhance efficiency, and harness the power of AI-driven transcription to unlock the potential of your handwritten content.',
-};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
